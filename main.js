36309391ae2144e080cd4e914feeeedac1f43a09
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-01-25 18:54:07 -0500

    Feat: Add remaining parts of game function to play through multiple rounds and declare winner

--- main.js
+++ main.js
@@ -1,47 +1,64 @@
 // Create array possibleChoices with pool of possible plays
 const possibleChoices = ['ROCK', 'PAPER', 'SCISSORS'];
 
-// Write function getComputerCode to randomly return either 'Rock,' 'Paper,' or 'Scissors'
-// Return string at randomized index position in possibleChoices
+// Randomly returns either 'Rock,' 'Paper,' or 'Scissors' by returning string at randomized index position in possibleChoices
 function getComputerChoice() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-// Gets computer choice and stores in variable
-const computerSelection = getComputerChoice();
 
-// Write function that plays a single round and takes playerSelection and computerSelection as parameters
-// Return string declaring winner of round
+let computerSelection; // Stores computer selection for each round
+let playerScore = 0; // Keeps track of number of rounds player wins
+let computerScore = 0; // Keeps track of number of rounds computer wins
+let playerSelection; // Holds player response to prompt for play
 
-// If playerSelection != Rock, Paper, or Scissors, return "That's not a valid choice. Please try again."
-// If playerSelection = computerSelection, return "It's a draw! Try again!"
-// If playerSelection = 'Rock' and computerSelection = 'Paper', return 'You lost! Paper beats rock!'
-// If playerSelection = 'Rock' and computerSelection = 'Scissors', return 'You won! Rock beats scissors!'
-// If playerSelection = 'Paper' and computerSelection = 'Rock', return 'You won! Paper beats rock!'
-// If playerSelection = 'Paper' and computerSelection = 'Scissors', return 'You lost! Scissors beat paper!
-// If playerSelection = 'Scissors' and computerSelection = 'Rock', return 'You lost! Rock beats scissors!'
-// If playerSelection = 'Scissors' and computerSelection = 'Paper', return 'You won! Scissors beat paper!'
+// game Function:
+// Declare variable to hold outcome of each round
+// If playerSelection is not rock, paper, or scissors, outcome is "That's not a valid choice. Please try again."
+// If playerSelection and computerSelection are the same, outcome is "It's a draw! Try again!"
+// If any of below win cases is true
+    // player chooses rock and computer chooses scissors
+    // player chooses paper and computer chooses rock
+    // player chooses scissors and computer chooses paper
+// Then outcome is "You won! playerSelection beats computerSelection!"
+// Otherwise, outcome is "You lost! computerSelection beats playerSelection!"
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection != 'ROCK' && playerSelection != 'PAPER' && playerSelection != 'SCISSORS') {
-        return "That's not a valid choice. Please try again.";
-    } else if (playerSelection == computerSelection) {
-        return "It's a draw! Try again!";
-    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
-        return "You lost! Paper beats rock!";
-    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
-        return "You won! Rock beats scissors!";
-    } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
-        return "You won! Paper beats rock!";
-    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
-        return "You lost! Scissors beat paper!";
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-        return "You lost! Rock beats scissors!";
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
-        return "You won! Scissors beats paper!";
+// Loop through prompting player and running playRound until player or computer wins 3 rounds (best of 5)
+// Log player and computer scores to console
+// Declare winner of the most rounds
+
+
+function game() {
+    let outcome;
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection != 'ROCK' && playerSelection != 'PAPER' && playerSelection != 'SCISSORS') {
+                outcome = "That's not a valid choice. Please try again.";
+        } else if (playerSelection == computerSelection) {
+                outcome = "It's a draw! Try again!";
+        } else if ((playerSelection == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection == 'PAPER' && computerSelection == 'ROCK') || (playerSelection == 'SCISSORS' && computerSelection == 'PAPER')) {
+            outcome = 'You won! ' + playerSelection + ' beats ' + computerSelection + '!';
+            playerScore += 1;
+        } else {
+            outcome = 'You lost! ' + computerSelection + ' beats ' + playerSelection + '!';
+            computerScore += 1;
+        }
+        console.log(outcome);
+    }
+
+    while (playerScore < 3 && computerScore < 3) {
+        computerSelection = getComputerChoice();
+        // console.log(computerSelection);
+        playerSelection = prompt('Pick rock, paper, or scissors').toUpperCase();
+        playRound(playerSelection, computerSelection);
+        
+        console.log(playerScore, computerScore);
+    }
+
+    if (playerScore < computerScore) {
+        console.log('You lost. Refresh the page to try again.');
+    } else {
+        console.log('You won! Great job!');
     }
 }
 
-const testChoice = 'paper';
-const playerSelection = testChoice.toUpperCase();
-console.log(playRound(playerSelection, computerSelection));
+game();
