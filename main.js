f97664281ac4853e6b57ff7397ee1c1d575f82b9
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-06 21:17:38 -0500

    Feat: Limit number of rounds played to 5 and display final results in UI

--- main.js
+++ main.js
@@ -14,26 +14,36 @@ let computerSelection; // Stores computer selection for each round
 let computerScore = 0; // Keeps track of number of rounds computer wins
 let playerSelection; // Holds player response to prompt for play
 let playerScore = 0; // Keeps track of number of rounds player wins
+let matchCount = 0;
+let finalOutcomeEl = document.getElementById('final-outcome');
 
 const choices = document.getElementById('btn-container');
 
 choices.addEventListener('click', e => {
     let outcome;
-	if (e.target.matches('button')) {
-		const button = e.target;
-		playerSelection = button.textContent.toUpperCase();
-        computerSelection = getComputerSelection();
-        if (playerSelection === computerSelection) {
-            outcome = "It's a draw! Try again!";
-        } else if ((playerSelection == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection == 'PAPER' && computerSelection == 'ROCK') || (playerSelection == 'SCISSORS' && computerSelection == 'PAPER')) {
-            outcome = `You won! ${playerSelection} beats ${computerSelection}!`;
-            playerScore += 1;
-        } else {
-            outcome = `You lost! ${computerSelection} beats ${playerSelection}!`;
-            computerScore += 1;
+    if (matchCount < 5) {
+        if (e.target.matches('button')) {
+            const button = e.target;
+            playerSelection = button.textContent.toUpperCase();
+            computerSelection = getComputerSelection();
+            if (playerSelection === computerSelection) {
+                outcome = "It's a draw! Try again!";
+            } else if ((playerSelection == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection == 'PAPER' && computerSelection == 'ROCK') || (playerSelection == 'SCISSORS' && computerSelection == 'PAPER')) {
+                outcome = `You won! ${playerSelection} beats ${computerSelection}!`;
+                playerScore += 1;
+                matchCount += 1;
+            } else {
+                outcome = `You lost! ${computerSelection} beats ${playerSelection}!`;
+                computerScore += 1;
+                matchCount += 1;
+            }
+            roundOutcome.textContent = outcome;
+            playerScoreEl.textContent = playerScore;
+            computerScoreEl.textContent = computerScore;
         }
-        roundOutcome.textContent = outcome;
-        playerScoreEl.textContent = playerScore;
-        computerScoreEl.textContent = computerScore;
-	}
+    } else if (playerScore > computerScore) {
+        finalOutcomeEl.textContent = 'You won! Great job!';
+    } else if (playerScore < computerScore) {
+        finalOutcomeEl.textContent = 'You lost. Refresh the page to try again.';
+    }
 })
