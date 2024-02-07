b760a499d0e4b66ce760c78d2a9f01864ce16b25
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-06 20:14:14 -0500

    Feat: Add elements to display round outcome, player score, and computer score and script to show respective variables in them each round

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
 // Create array possibleChoices with pool of possible plays
 const possibleChoices = ['ROCK', 'PAPER', 'SCISSORS'];
 
+const roundOutcome = document.getElementById('round-outcome');
+const playerScoreEl = document.getElementById('player-score');
+const computerScoreEl = document.getElementById('computer-score');
+
 // Randomly returns either 'Rock,' 'Paper,' or 'Scissors' by returning string at randomized index position in possibleChoices
 function getComputerSelection() {
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-
 let computerSelection; // Stores computer selection for each round
-let playerScore = 0; // Keeps track of number of rounds player wins
 let computerScore = 0; // Keeps track of number of rounds computer wins
 let playerSelection; // Holds player response to prompt for play
-
-// game Function:
-// Declare variable to hold outcome of each round
-// If playerSelection is not rock, paper, or scissors, outcome is "That's not a valid choice. Please try again."
-// If playerSelection and computerSelection are the same, outcome is "It's a draw! Try again!"
-// If any of below win cases is true
-    // player chooses rock and computer chooses scissors
-    // player chooses paper and computer chooses rock
-    // player chooses scissors and computer chooses paper
-// Then outcome is "You won! playerSelection beats computerSelection!"
-// Otherwise, outcome is "You lost! computerSelection beats playerSelection!"
-
-// Loop through prompting player and running playRound until player or computer wins 3 rounds (best of 5)
-// Log player and computer scores to console
-// Declare winner of the most rounds
-
+let playerScore = 0; // Keeps track of number of rounds player wins
 
 const choices = document.getElementById('btn-container');
 
@@ -45,6 +32,8 @@ choices.addEventListener('click', e => {
             outcome = `You lost! ${computerSelection} beats ${playerSelection}!`;
             computerScore += 1;
         }
-        console.log(outcome);
+        roundOutcome.textContent = outcome;
+        playerScoreEl.textContent = playerScore;
+        computerScoreEl.textContent = computerScore;
 	}
 })
